055628d295eaad11bbc820bb63ae0a176976ef18
ThuWangzw <wangzw834@163.com>
2022-01-06 16:20:47 +0800

    feat: change to static serve

--- next.config.js
+++ next.config.js
@@ -58,6 +58,9 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
   },
+  images: {
+    loader: 'custom',
+  },
   async headers() {
     return [
       {
